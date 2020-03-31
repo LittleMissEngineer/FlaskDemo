11a0ac600b393ee2629a8209f98f33e717f6192c
Melanie Moore <littlemsengineer@gmail.com>
2020-03-31 00:48:37 -0400

    Updated windchill calculator

--- app/windchill.py
+++ app/windchill.py
@@ -1,5 +1,6 @@
 import os
 import math
+import json
 
 
 from flask import Flask, request, redirect, render_template, session, url_for, g
@@ -11,27 +12,88 @@ app.config.from_mapping(SECRET_KEY='devIAm')  # Needed for session tracking
 
 @app.route('/', methods=['GET','POST'])
 
+
+
 def tempCalculate():
   temp = ""
   speed = ""
+  temp_type = ""
+  speed_type = ""
+  windChill = None
+
+  
+  #get the values from the form
+
+  temp = request.args.get('temp')
+  speed = request.args.get('speed')
+  temp_type = request.args.get('temp_type')
+  speed_type = request.args.get('type_of_speed')
+  #speed = float(request.form["speed"])
   
 
-  if request.method == 'POST':
-temp    = float(input("Enter the air temperature in (degrees Celsius): "))
-speed   = float(input("Enter the wind speed (kilometer per hour): "))
 
-windchill = 13.12 + 0.6215*temp -  11.37*math.pow(speed, 0.16) + 0.3965*temp*math.pow(speed, 0.16)
 
-temp = float(request.form["temp"])
-speed = float(request.form["speed"])
+  #if temp_type == "c" && speed_type == "kph":
+      #temp = convertFahtoCel(temp)
+      #speed = convertMPHtoKPH(speed)
+  #windChill = calculateWindChill(temp, speed)
+
+
+#elif temp_type == "f" && speed_type == "mph":
+  #temp = copyright                 `toFah(temp)
+  #speed = convertKPHtoMPH(speed)
+  #windChill = calculateWindChill(temp, speed)
+
+#if 'json' request.args:
+  #return jsonify(windChill)
+
+
+
+
+
+
+ #def convertCeltoFah(F):
+  #  return (1.8 * temp) + 32
+
+  #def convertFahtoCel(C):
+  # return(temp - 32) /1.8
+
+  #def convertMPHtoKPH(KPH):
+  # return speed * 1.609344
+
+    #def convertKPHtoMPH(MPH):
+  # return kph * 0.62137
+
+    #def calculateWindchill(t , v)
+      #return 13.12 + 0.6215*t -  11.37*math.pow(v, 0.16) + 0.3965*t*math.pow(v, 0.16)
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   
   # Update the number of visits
   # session is a dict which persists.  Stored in client cookie (no local storage)
-  if 'times' not in session:
-    session['times'] = 0
-  session['times'] += 1
 
-  return render_template('windchill.html',times = session['times'] , temp = temp, speed = speed , windchill = windchill) # Send t to the template
+#if 'times' not in session:
+ #   session['times'] = 0
+  #session['times'] += 1
+
+  return render_template('windchill.html', temp = temp, speed = speed, temp_type = temp_type, speed_type = speed_type, windChill = windChill) # Send t to the template
 
 @app.route('/logout')
 def logout():
