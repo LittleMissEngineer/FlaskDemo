1521b24560104075e84c2d1aa35d945dcfb1bf61
Melanie Moore <littlemsengineer@gmail.com>
2020-04-04 23:37:25 -0400

    Fixed the errors with the wind chill calc

--- app/windchill.py
+++ app/windchill.py
@@ -3,69 +3,83 @@ import math
 import json
 
 
-from flask import Flask, request, redirect, render_template, session, url_for, g
+from flask import Flask, request, redirect, render_template, flash, session, url_for, g
 
 app = Flask(__name__)
 app.config.from_mapping(SECRET_KEY='devIAm')  # Needed for session tracking
   # Note flask does CLIENT session data storage!  Watch data sizes!
 
 
+@app.route('/')
+def form():
+  return render_template('windchill.html')
+
+
 @app.route('/', methods=['GET','POST'])
-
-
-
 def tempCalculate():
-  temp = ""
-  speed = ""
+  temp = 0.0
+  speed = 0.0
   temp_type = ""
   speed_type = ""
+  temp_option = None
+  speed_option = None
   windChill = None
 
   
   #get the values from the form
 
-  temp = request.args.get('temp')
-  speed = request.args.get('speed')
-  temp_type = request.args.get('temp_type')
-  speed_type = request.args.get('type_of_speed')
+  #temp = request.args.get('temp')
+  #temp = request.form['temp']
+  #speed = request.form['speed']
+  #temp_option = request.form['temp_option']
+  #speed_option = request.form['speed_option']
+  #temp_type = request.args.get('temp_type')
+  #speed_type = request.args.get('type_of_speed')
   #speed = float(request.form["speed"])
-  
 
+  if request.method == 'POST':
+    if 'submit_button' in request.form:
+      temp = float(request.form['temp'])
+      speed = float(request.form['speed'])
+      temp_option = request.args.get('temp_option')
+      speed_option = request.args.get('speed_option')
+      #return speed_option
 
-
-  #if temp_type == "c" && speed_type == "kph":
-      #temp = convertFahtoCel(temp)
-      #speed = convertMPHtoKPH(speed)
-  #windChill = calculateWindChill(temp, speed)
-
-
-#elif temp_type == "f" && speed_type == "mph":
-  #temp = copyright                 `toFah(temp)
-  #speed = convertKPHtoMPH(speed)
-  #windChill = calculateWindChill(temp, speed)
+      if temp_option == "c":
+        temp = convertFahtoCel(temp)
+        if speed_option == "kph":
+          speed = convertMPHtoKPH(speed)
+          windChill = calculateWindchill(temp, speed)
+          #return float(windChill)
+      elif temp_option == "f":
+          temp = convertCeltoFah(temp)
+          if speed_option == "mph":
+            speed = convertKPHtoMPH(speed)
+            windChill = calculateWindchill(temp, speed)
+            
 
 #if 'json' request.args:
   #return jsonify(windChill)
 
+      return render_template('windchill.html', temp = temp, speed = speed, temp_type = temp_type, temp_option = temp_option, speed_option = speed_option) # Send t to the template
 
 
 
 
+def convertCeltoFah(temp):
+  return (1.8 * temp) + 32
 
- #def convertCeltoFah(F):
-  #  return (1.8 * temp) + 32
+def convertFahtoCel(temp):
+  return(temp - 32) /1.8
 
-  #def convertFahtoCel(C):
-  # return(temp - 32) /1.8
+def convertMPHtoKPH(speed):
+  return speed * 1.609344
 
-  #def convertMPHtoKPH(KPH):
-  # return speed * 1.609344
+def convertKPHtoMPH(speed):
+  return kph * 0.62137
 
-    #def convertKPHtoMPH(MPH):
-  # return kph * 0.62137
-
-    #def calculateWindchill(t , v)
-      #return 13.12 + 0.6215*t -  11.37*math.pow(v, 0.16) + 0.3965*t*math.pow(v, 0.16)
+def calculateWindchill(temp , speed):
+  return 13.12 + 0.6215*temp -  11.37*math.pow(speed, 0.16) + 0.3965*temp*math.pow(speed, 0.16)
 
   
 
@@ -93,7 +107,7 @@ def tempCalculate():
  #   session['times'] = 0
   #session['times'] += 1
 
-  return render_template('windchill.html', temp = temp, speed = speed, temp_type = temp_type, speed_type = speed_type, windChill = windChill) # Send t to the template
+  #return render_template('windchill.html', temp = temp, speed = speed, temp_type = temp_type, temp_option = temp_option, speed_option = speed_option) # Send t to the template
 
 @app.route('/logout')
 def logout():
